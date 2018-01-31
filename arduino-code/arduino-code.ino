f3c423327f16bd01416753a36586716d69549c33
aguskoll <aguskoll@gmail.com>
2018-01-30 21:00:27 -0300

    cambio la funcion para chequear si hay sms, ya no se usa mas la que esta deprecated.

--- arduino-code/arduino-code.ino
+++ arduino-code/arduino-code.ino
@@ -9,29 +9,28 @@
 #include "sms.h"
 SMSGSM sms;
 
+const int TEXT_SIZE = 160;
 int numdata;
 boolean started=false;
-char smsbuffer[200];
+char sms_text[TEXT_SIZE];
 char receptionNumber[20];
 char destinationNumber[20];
-char text[500];
 int NEW_TEXT = 0;
 int NEW_NUMBER = 0;
-char START_CHAR = '#';
-char END_CHAR = '$';
+const char START_NUMB = '#';
+const char END_NUMB = '$';
+const char START_TEXT = '%';
+const char END_TEXT = '&';
 
 void setup() 
 {
   //Serial connection.
   Serial.begin(9600);
-  Serial.println("GSM Shield testing.");
-  //Start configuration of shield with baudrate.
-  //For http uses is raccomanded to use 4800 or slower.
   if (gsm.begin(2400)){
-    Serial.println("\nstatus=READY");
+    Serial.println("#status=READY$");
     started=true;  
   }
-  else Serial.println("\nstatus=IDLE");
+  else Serial.println("#status=IDLE$");
 
 };
 
@@ -52,15 +51,16 @@ void readNumberFromSerial()
     if (Serial.available() > 0) 
     {
        data=Serial.read();
-       if(data == START_CHAR)//the beggining of the number
+       if(data == START_NUMB)//the beggining of the number
         { 
           pos = 0;
         }
         else 
-          if(data == END_CHAR) //the end of the numer 
+          if(data == END_NUMB) //the end of the numer 
           {
                   FLAG_END = 1;
                   NEW_NUMBER = 1; //activate the flag to send a mew SMS
+                  Serial.println(destinationNumber);
           }
           else
           {
@@ -73,6 +73,7 @@ void readNumberFromSerial()
         FLAG_END = 1;
       }
   }
+  Serial.flush();
 }
 
 /**
@@ -91,19 +92,21 @@ void readTextForSMS()
     {
        data=Serial.read();
        
-       if(data == START_CHAR)//the beggining of the text
+       if(data == START_TEXT)//the beggining of the text
         { 
           pos = 0; 
+          cleanBuffer();
         }
         else 
-            if(data == END_CHAR) //the end of the text 
+            if(data == END_TEXT) //the end of the text 
             {
                   FLAG_END = 1;
                   NEW_TEXT = 1;
+                   Serial.println(sms_text);
             }
             else
             {
-              text[pos] = data;
+              sms_text[pos] = data;
               pos++;
             }
       }
@@ -112,14 +115,20 @@ void readTextForSMS()
         FLAG_END = 1;
       }
   }
+  Serial.flush();
 }
 
+void cleanBuffer(){
+  int i;
+  for(i = 0; i <TEXT_SIZE;i++ )
+    sms_text[i] = ' ';   
+}
 /**
 Sends the sms with the text received from the serial to the number received.
 */
 void sendSMS()
 {
-  if (sms.SendSMS(destinationNumber,text)){
+  if (sms.SendSMS(destinationNumber,sms_text)){
     Serial.println("\n#SMS OK$");
     NEW_TEXT = 0; //no new sms
     NEW_NUMBER = 0;
@@ -130,23 +139,25 @@ void sendSMS()
 
 void loop() 
 {
+  char position;
   if(started){
-    if(gsm.readSMS(smsbuffer, 1000, receptionNumber, 20))
-    {
-      String number =START_CHAR+receptionNumber+END_CHAR; 
-      Serial.println(number);
-      String texto = START_CHAR+smsbuffer+END_CHAR;
-      Serial.println(texto);
-    }
+    position = sms.IsSMSPresent(SMS_UNREAD);
+    if(position)
+      //if(gsm.readSMS(smsbuffer, 200, receptionNumber, 20))
+      {
+        sms.GetSMS(position, receptionNumber, sms_text, TEXT_SIZE);
+        String number =START_TEXT+receptionNumber+END_TEXT; 
+        Serial.println(number);
+        String texto = START_TEXT+sms_text+END_TEXT;
+        Serial.println(texto);
+      }
     delay(1000);
     if(NEW_NUMBER == 0)
       readNumberFromSerial();
-    if(NEW_TEXT == 0)
+    if(NEW_TEXT == 0 && NEW_NUMBER == 1)
       readTextForSMS();
     if(NEW_TEXT == 1 and NEW_NUMBER == 1)
     {
-      Serial.println(destinationNumber);
-      Serial.println(text);
       sendSMS();
     }
   }
